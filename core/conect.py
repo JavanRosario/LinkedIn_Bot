5de7d6dd62dff246f175b167d5331a68a86c7fef
JavanRosario <oliveiraajavan@hotmail.com>
2025-08-07 11:56:20 -0300

    ajuste log

--- core/conect.py
+++ core/conect.py
@@ -3,8 +3,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import ElementClickInterceptedException
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 
 from time import sleep
 
@@ -17,18 +15,16 @@ class ConectDevs(LinkedInLogin):
 
     def conect(self):
         buttons = self.driver.find_elements(
-            By.XPATH, '//span[normalize-space(text())="Conectar"]')
-        element = self.driver.find_element(
-            By.XPATH, '/html/body/div/dialog/div/div/div/div/section/div/div/div')
-        actions = ActionChains(self.driver)
-        for button in buttons:
+            By.XPATH, './/span[normalize-space(text())="Conectar"]')
+        for i, button in enumerate(buttons):
             try:
                 WebDriverWait(self.driver, TIME_TO_SLEEP).until(
                     EC.visibility_of(button))
                 sleep(5)
                 button.click()
-                actions.move_to_element(button).click(
-                ).send_keys(Keys.PAGE_DOWN).perform()
+                if i == len(buttons) - 1:
+                    print("✅ Fim do loop — último botão clicado.")
+                    sleep(10)
             except ElementClickInterceptedException:
                 self.driver.execute_script("arguments[0].click();", button)
             except Exception as e:
